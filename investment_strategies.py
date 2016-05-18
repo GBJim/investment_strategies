2fa87e05d6a117920f0e3e956086ae1b86681fa5
gb <ccha97u@gmail.com>
2016-05-18 13:20:52 +0800

    Basic three stratagies

--- investment_strategies.py
+++ investment_strategies.py
@@ -21,27 +21,41 @@ class Strategy(object):
 		self.weight[1:] = np.random.rand(degree)
 
 
-	def get_K(self, weighted_desnity, goal, money ):
+	def normalize(self,weighted_desnity):
+
+		minimum = np.min(weighted_desnity)
+		if minimum <= 0:
+			weighted_desnity += (1-minimum)
+
+		return weighted_desnity
+
+
+
+	def get_K(self, vector_k, goal, money ):
 		k_range = goal - money
-
-
-		probability_desnity = weighted_desnity[0:k_range] / np.sum(weighted_desnity[0:k_range])
+		weighted_desnity = np.dot(self.weight.T, vector_k[:, :k_range])
+		weighted_desnity = self.normalize(weighted_desnity)
+		#print(weighted_desnity)
+		#print(k_range)
+		probability_desnity = weighted_desnity / np.sum(weighted_desnity)
+		#print(probability_desnity )
+		#print(k_range)
 		choice = np.random.choice(k_range,1, p=probability_desnity)[0]
 
 
 		return choice+1
 		
 
-	def get_weighted_density(self, goal):
+	def get_vector_k(self, goal):
 		degree = self.degree
-		weighted_desnity = np.ones((degree+1, goal),dtype=int)
+		vector_k = np.ones((degree+1, goal),dtype=int)
 
 		for i in range(1, degree+1):
-			weighted_desnity[i,:] = np.arange(1, goal+1, dtype=int)
-			weighted_desnity[i,:] *=  weighted_desnity[i-1,:] 
-		weighted_desnity = np.dot(self.weight.T, weighted_desnity)
+			vector_k[i,:] = np.arange(0, goal, dtype=int)
+			vector_k[i,:] *=  vector_k[i-1,:] 
+		#weighted_desnity = np.dot(self.weight.T, weighted_desnity)
 
-		return weighted_desnity
+		return vector_k
 
 	def get_average_steps(self,goal=100, repetitions=100000):
 		total_steps = 0
@@ -55,14 +69,15 @@ class Strategy(object):
 		steps = 0
 		money = 0
 		
-
-		weighted_desnity = self.get_weighted_density(goal)
+		vector_k = self.get_vector_k(goal-money)
+		#weighted_desnity = self.get_weighted_density(goal)
 
 		#print(weighted_desnity)
 
 
 		while(money < goal):
-			k = self.get_K(weighted_desnity, goal, money)
+
+			k = self.get_K(vector_k, goal, money)
 			reward = invest(k)
 			steps += 1 
 			money += reward
@@ -75,14 +90,44 @@ class Strategy(object):
 
 
 
-s = Strategy(degree=3)
-
-#result = s.start_investment(100)
-print(s.get_average_steps())
-#print(result["history"])
-#print(result["total_steps"])
+s = Strategy(degree=1)
 
 
+goal = 1000
+repetitions = 10**5
+
+
+# F(x) = a + bx
+
+
+
+
+a = 1
+b = 1
+s.weight[0] = a
+s.weight[1] = b
+
+print("\nRisky Strategy Average Steps:")
+
+
+print(s.get_average_steps(goal, repetitions))
+
+a = 1
+b = 0
+s.weight[0] = a
+s.weight[1] = b
+#print(s.get_weighted_density(10))
+print("\nUniform Strategy Average Steps:")
+print(s.get_average_steps(goal, repetitions))
+
+a = 0
+b = -1
+s.weight[0] = a
+s.weight[1] = b
+#print(s.get_weighted_density(10))
+print("\nSafe Strategy Average Steps:")
+
+print(s.get_average_steps(goal, repetitions))
 
 
 
