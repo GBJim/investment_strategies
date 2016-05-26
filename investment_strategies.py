71b508669f59506c40c0a02a903843721370c967
gb <ccha97u@gmail.com>
2016-05-26 21:32:46 +0800

    1.Fixing the bug that Strategy object wrongly access global variable s  2.Strategy object now can select K by customized decision function 3.Finish thye steps_distribution charts

--- investment_strategies.py
+++ investment_strategies.py
@@ -1,7 +1,7 @@
 from random import randint
 import numpy as np
 
-invest = lambda k: k if randint(1,k) == 1 else 0
+invest = lambda k: np.random.choice([0,k],1,p=[1-(1/k),1/k])[0]
 
 def get_exptection(k,times=100):
 	earn = 0
@@ -15,16 +15,29 @@ def get_exptection(k,times=100):
 
 class Strategy(object):
 
-	def __init__(self, degree=1, parameters = None):
-		self.degree = degree
-		self.weight = np.ones(degree+1, dtype=float)
-		if parameters is None:
-			self.weight = np.random.rand(degree+1)
+	def __init__(self, degree=None, parameters=None, customized_decision=None):
+		
+		if degree is None:
+			self.degree = None
+			if not customized_decision is None:
+				self.decision_function = customized_decision
+			else: 
+				self.decision_function = self.get_K
 
-		elif len(parameters) == degree + 1:
-			self.weight = np.array(parameters)
 
 
+		else:
+			self.degree = degree
+			self.weight = np.ones(degree+1, dtype=float)
+			if parameters is None:
+				self.weight = np.random.rand(degree+1)
+
+			elif len(parameters) == degree + 1:
+				self.weight = np.array(parameters)
+
+			self.decision_function = self.get_K
+
+			
 	def normalize(self,weighted_desnity):
 
 		minimum = np.min(weighted_desnity)
@@ -35,7 +48,7 @@ class Strategy(object):
 
 
 
-	def get_K(self, vector_k, goal, money ):
+	def get_K(self, goal, money, vector_k ):
 		k_range = goal - money
 		weighted_desnity = np.dot(self.weight.T, vector_k[:, :k_range])
 		weighted_desnity = self.normalize(weighted_desnity)
@@ -51,6 +64,9 @@ class Strategy(object):
 		
 
 	def get_vector_k(self, goal):
+		if self.degree is None:
+			return None
+
 		degree = self.degree
 		vector_k = np.ones((degree+1, goal),dtype=int)
 
@@ -82,12 +98,16 @@ class Strategy(object):
 		history = []
 		steps = 0
 		money = 0
+		
 		vector_k = self.get_vector_k(goal-money)
+
+		
+
 
 
 		while(money < goal):
 
-			k = self.get_K(vector_k, goal, money)
+			k = self.decision_function(goal, money, vector_k )
 			reward = invest(k)
 			steps += 1 
 			money += reward
@@ -110,6 +130,13 @@ class Strategy(object):
 		w.close()
 		print("Output file {} has been writen.".format(write_file_name))
 
+	def get_multiple_steps(self,goal, repetitions):
+		steps = []
+		for i in range(repetitions):
+			result = self.start_investment(goal)
+			steps.append(result["total_steps"])
+
+		return steps
 
 
 
@@ -125,63 +152,23 @@ b = 1
 parameters= [a,b]
 s = Strategy(degree, parameters)
 
-
+if  __name__ == '__main__':
 
 # We set goal as 10 for the following examples
-goal = 10
+	goal = 10
 
 #Simulate an investment:
-result = s.start_investment(goal)
+	result = s.start_investment(goal)
 
 #Get the total steps form the investment result and historical records as well
-print(result["total_steps"])
-print(result["history"])
+	print(result["total_steps"])
+	print(result["history"])
 
 #You can also write the historical records into a file. CSV format is provided
-write_file_name = "my_investment_record.csv"
-s.start_investment(goal, write_file_name)
-
+	write_file_name = "my_investment_record.csv"
+	s.start_investment(goal, write_file_name)
 
 
 #Calculate the average steps for a given repetitions: 100
-print(s.get_average_steps(goal, 100))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-print("\nRisky Strategy Average Steps:")
-
-
-print(s.get_average_steps(goal, repetitions))
-
-a = 1
-b = 0
-s.weight[0] = a
-s.weight[1] = b
-#print(s.get_weighted_density(10))
-print("\nUniform Strategy Average Steps:")
-print(s.get_average_steps(goal, repetitions))
-
-a = 0
-b = -1
-s.weight[0] = a
-s.weight[1] = b
-#print(s.get_weighted_density(10))
-print("\nSafe Strategy Average Steps:")
-
-print(s.get_average_steps(goal, repetitions))
-'''
-
-
+	print(s.get_average_steps(goal, 100))
 
